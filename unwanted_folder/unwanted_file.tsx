b9a36487be8d71fb6a5a7e298ca0bd4ba40f3e6d
Mano Mangaldas <mano.mangaldas@atimaterials.com>
2024-11-25 07:56:33 -0500

    commiting local changes

--- unwanted_folder/unwanted_file.tsx
+++ unwanted_folder/unwanted_file.tsx
@@ -3,7 +3,7 @@ import React from "react";
 type Props = {};
 
 const unwanted_file = (props: Props) => {
-  return <div>unwanted_file</div>;
+  return <div>unwanted_file dfgdf fdg </div>;
 };
 
 export default unwanted_file;
