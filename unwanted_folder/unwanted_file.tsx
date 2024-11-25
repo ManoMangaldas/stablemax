7e08f0cd39eecb421e24b075c7388da2b7dfdc0b
Mano Mangaldas <44945556+ManoMangaldas@users.noreply.github.com>
2024-11-25 08:04:59 -0500

    Delete unwanted_folder directory

--- unwanted_folder/unwanted_file.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const unwanted_file = (props: Props) => {
-  return <div>unwanted_file dfgdf fdgv sdfs sf </div>;
-};
-
-export default unwanted_file;
